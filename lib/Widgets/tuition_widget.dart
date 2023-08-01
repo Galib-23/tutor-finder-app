4468e1e1b6449e8d8a90a09a791bf077f94f2dbe
Galib-23 <aagalib2323@gmail.com>
2023-08-01 01:31:35 -0700

    tuition filtering tested and found good

--- lib/Widgets/tuition_widget.dart
+++ lib/Widgets/tuition_widget.dart
@@ -1,4 +1,8 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:tutor_finder/Services/global_methods.dart';
 
 class TuitionWidget extends StatefulWidget {
   final String subject;
@@ -29,6 +33,59 @@ class TuitionWidget extends StatefulWidget {
 }
 
 class _TuitionWidgetState extends State<TuitionWidget> {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  _deleteDialog() {
+    User? user = _auth.currentUser;
+    final _uid = user!.uid;
+    showDialog(
+        context: context,
+        builder: (ctx) {
+          return AlertDialog(
+            actions: [
+              TextButton(
+                onPressed: () async {
+                  try {
+                    if (widget.uploadedBy == _uid) {
+                      await FirebaseFirestore.instance
+                          .collection('tuitions')
+                          .doc(widget.tuitionId)
+                          .delete();
+                      await Fluttertoast.showToast(
+                        msg: 'Tuition Has Been Deleted',
+                        toastLength: Toast.LENGTH_LONG,
+                        backgroundColor: Colors.grey,
+                        fontSize: 18.0,
+                      );
+                      // ignore: use_build_context_synchronously
+                      Navigator.canPop(context) ? Navigator.pop(context) : null;
+                    } else {
+                      GlobalMethod.showErrorDialog(
+                          error: 'You Cannot Perform This Action!', ctx: ctx);
+                    }
+                  } catch (error) {
+                    GlobalMethod.showErrorDialog(
+                        error: 'This Cannot Be Deleted', ctx: ctx);
+                  } finally {}
+                },
+                child: const Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(
+                      Icons.delete,
+                      color: Colors.red,
+                    ),
+                    Text(
+                      'Delete',
+                      style: TextStyle(color: Colors.red),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          );
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Card(
@@ -37,7 +94,9 @@ class _TuitionWidgetState extends State<TuitionWidget> {
       margin: const EdgeInsets.symmetric(horizontal: 10, vertical: 8),
       child: ListTile(
         onTap: () {},
-        onLongPress: () {},
+        onLongPress: () {
+          _deleteDialog();
+        },
         contentPadding:
             const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
         leading: Container(
